1d61e2407c1121e2636e2f822ee9ca1740ba146c
basdxz <tudurap.com@gmail.com>
2021-04-07 11:02:06 +0100

    Adjust IR var names

--- IR_SENSOR_TEST/IR_SENSOR_TEST.ino
+++ IR_SENSOR_TEST/IR_SENSOR_TEST.ino
@@ -6,13 +6,13 @@
 #define IR_PIN_A A0
 // For Model SHARP GP2Y0A41SK0F
 #define MODEL_A 430
-SharpIR IR_SENSOR_A(IR_PIN_A, MODEL_A);
+SharpIR irSensor_A(IR_PIN_A, MODEL_A);
 
 // B is the 100 to 550 CM distance sensor
 #define IR_PIN_B A1
 // For Model SHARP GP2Y0A710K0F
 #define MODEL_B 100550
-SharpIR IR_SENSOR_B(IR_PIN_B, MODEL_B);
+SharpIR irSensor_B(IR_PIN_B, MODEL_B);
 
 void setup() {
   Serial.begin(9600);
@@ -38,10 +38,10 @@ void testIRSensors() {
 
 void testIRSensorA() {
   Serial.print("Sensor A Distance (cm): ");
-  Serial.println(IR_SENSOR_A.distance());
+  Serial.println(irSensor_A.distance());
 }
 
 void testIRSensorB() {
   Serial.print("Sensor B Distance (cm): ");
-  Serial.println(IR_SENSOR_B.distance());
+  Serial.println(irSensor_B.distance());
 }
