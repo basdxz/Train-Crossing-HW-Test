50aaae8ce870c916d511e257114fc0c38b28ae8c
basdxz <tudurap.com@gmail.com>
2021-04-07 14:07:21 +0100

    Update Buzzer formatting

--- BUZZER_TEST/BUZZER_TEST.ino
+++ BUZZER_TEST/BUZZER_TEST.ino
@@ -1,28 +1,39 @@
 // Must match baud on the serial monitor
 #define BAUD_RATE 9600
-
-#define BUZZER_PIN 5 // Connect through a 100 Ohm resistor
+// Connect through a 100 Ohm resistor
+#define BUZZER_PIN 5
 
 void setup(){
-	Serial.begin(BAUD_RATE);
+	setupSerial();
 	setupBuzzer();
-}
-
-void setupBuzzer(){
-	pinMode(BUZZER_PIN, OUTPUT);
 	testBuzzer();
 }
 
 void loop() {
 }
 
-void testBuzzer() {
+void setupSerial() {
+	Serial.begin(BAUD_RATE);
 	Serial.println("-====-====-====-====-");
+	Serial.print("Serial Started at ");
+	Serial.print(BAUD_RATE);
+	Serial.println(" Baud");
+	Serial.println("-====-====-====-====-");
+}
+
+void setupBuzzer(){
+	pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void testBuzzer() {
 	Serial.println("Starting buzzer test");
 	Serial.println("-====-====-====-====-");
-	for (int i = 0; i < 5; i++) {
-		tone(BUZZER_PIN, 200, 200); // 200Hz tone for 200 ms
-		Serial.println("Buzz!");
+	for (int i = 1; i <= 5; i++) {
+		int freq = 200 * i;
+		tone(BUZZER_PIN, freq, 250); // 200 * i Hz tone for 250 ms
+		Serial.print("Buzz at ");
+		Serial.print(freq);
+		Serial.println(" Hz");
 		Serial.println("-====-====-====-====-");
 		delay(500);
 	}
