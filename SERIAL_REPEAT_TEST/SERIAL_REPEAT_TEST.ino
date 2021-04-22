f6d4f4009f395a844de67ea9fb20382268ff33b9
basdxz <tudurap.com@gmail.com>
2021-04-22 14:07:18 +0100

    Update Serial Repeat
    Added String comparisons

--- SERIAL_REPEAT_TEST/SERIAL_REPEAT_TEST.ino
+++ SERIAL_REPEAT_TEST/SERIAL_REPEAT_TEST.ino
@@ -1,5 +1,7 @@
 // Must match baud on the serial monitor
 #define BAUD_RATE 9600
+#define TEST_MESSAGE "raise"
+#define TEST_RESPONSE "barrier raised!"
 
 void setup() {
 	setupSerial();
@@ -21,10 +23,19 @@ void setupSerial() {
 void testSerialRead() {
 	Serial.println("Testing serial read");
 	Serial.println("-====-====-====-====-");
+	Serial.print("Testing string is: ");
+	Serial.println(TEST_MESSAGE);
+	Serial.println("-====-====-====-====-");
 	Serial.println("Waiting for user string");
 	Serial.println("-====-====-====-====-");
 	while (!Serial.available());
-	Serial.print("User string was: ");
-	Serial.print(Serial.readString()); // Expects a string to be sent, followed by a \n (enter key)
-	Serial.println("-====-====-====-====-");
+	Serial.print("User string was: \"");
+	String message = Serial.readString(); // Expects a string to be sent, followed by a \n (enter key)
+	message = message.substring(0, message.length() - 1);
+	Serial.print(message);
+	Serial.println("\"\n-====-====-====-====-");
+	if (message == TEST_MESSAGE) {
+		Serial.println(TEST_RESPONSE);
+		Serial.println("-====-====-====-====-");
+	}
 }
